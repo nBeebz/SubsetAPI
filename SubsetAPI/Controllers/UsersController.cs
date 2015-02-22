1c56336f48ab936cedd4dd81ed0a846e7301f442
Nav Bhatti <bhatti.nav@gmail.com>
2015-02-22 02:12:50 +0000

    Added Subset EntityFramework. UsersController WebAPI working
    Signed-off-by: Nav Bhatti <bhatti.nav@gmail.com>

---
+++ SubsetAPI/Controllers/UsersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SubsetAPI.Models;
+
+namespace SubsetAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // GET: api/Users
+        public IQueryable<Users> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult GetUsers(string id)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUsers(string id, Users users)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != users.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult PostUsers(Users users)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(users);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsersExists(users.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = users.id }, users);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult DeleteUsers(string id)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(users);
+            db.SaveChanges();
+
+            return Ok(users);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsersExists(string id)
+        {
+            return db.Users.Count(e => e.id == id) > 0;
+        }
+    }
+}
