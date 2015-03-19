eeaa6092b9ce5b14073e79096b0d7e67379a4ecc
Nav Bhatti <bhatti.nav@gmail.com>
2015-03-18 22:10:52 -0700

    enabled CORS
    Signed-off-by: Nav Bhatti <bhatti.nav@gmail.com>

--- SubsetAPI/Controllers/UsersController.cs
+++ SubsetAPI/Controllers/UsersController.cs
@@ -9,9 +9,12 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using SubsetAPI.Models;
+using System.Web.Http.Cors;
 
 namespace SubsetAPI.Controllers
 {
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
         private Entities db = new Entities();
